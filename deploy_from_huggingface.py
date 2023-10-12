0886044de8299d9e98c322e80c4eaf59a0371cdc
LukasBahrenberg <lukas.bahrenberg@gmail.com>
2023-10-12 13:39:28 +0200

    update

--- deploy_from_huggingface.py
+++ deploy_from_huggingface.py
@@ -1,44 +1,41 @@
 import os
 from dotenv import load_dotenv
-
-from transformers import AutoModel, AutoTokenizer
-
-import sagemaker
-from sagemaker import get_execution_role
-from sagemaker.model import Model
-from sagemaker.utils import name_from_base
-from sagemaker.image_uris import retrieve
-
 load_dotenv()
+import json
+import sagemaker
+import boto3
+from sagemaker.huggingface import HuggingFaceModel, get_huggingface_llm_image_uri
 
-HUGGING_FACE_ACCESS_KEY = os.environ.get('HUGGING_FACE_ACCESS_KEY')
+try:
+	role = sagemaker.get_execution_role()
+except ValueError:
+	iam = boto3.client('iam')
+	role = iam.get_role(RoleName='sagemaker_execution_role')['Role']['Arn']
 
-model_name = "meta-llama/Llama-2-7b-chat-hf"  # replace with your model name
+# Hub Model configuration. https://huggingface.co/models
+hub = {
+	'HF_MODEL_ID':'meta-llama/Llama-2-7b-chat-hf',
+	'SM_NUM_GPUS': json.dumps(1),
+	'HUGGING_FACE_HUB_TOKEN': os.environ.get('HUGGING_FACE_HUB_TOKEN'),
+}
 
-model = AutoModel.from_pretrained(model_name, token=HUGGING_FACE_ACCESS_KEY)
-tokenizer = AutoTokenizer.from_pretrained(model_name, token=HUGGING_FACE_ACCESS_KEY)
+assert hub['HUGGING_FACE_HUB_TOKEN'] != '<REPLACE WITH YOUR TOKEN>', "You have to provide a token."
 
-model.save_pretrained("./model")
-tokenizer.save_pretrained("./model")
-
-sagemaker_session = sagemaker.Session()
-role = get_execution_role()
-
-bucket = sagemaker_session.default_bucket()
-prefix = 'your-prefix'  # e.g., 'huggingface-model'
-
-model_artifact = sagemaker_session.upload_data(path='./model', bucket=bucket, key_prefix=prefix)
-
-region = sagemaker.Session().boto_region_name
-pytorch_container = retrieve('pytorch', region, version='1.9.0')  # Replace '1.9.0' with your desired version
-
-pytorch_model = Model(
-    model_data=model_artifact,
-    role=role,
-    image_uri=pytorch_container,
-    entry_point='inference.py',
-    source_dir='code',
-    sagemaker_session=sagemaker_session
+# create Hugging Face Model Class
+huggingface_model = HuggingFaceModel(
+	image_uri=get_huggingface_llm_image_uri("huggingface",version="1.1.0"),
+	env=hub,
+	role=role, 
 )
 
-predictor = pytorch_model.deploy(instance_type='ml.m5.large', initial_instance_count=1)
+# deploy model to SageMaker Inference
+predictor = huggingface_model.deploy(
+	initial_instance_count=1,
+	instance_type="ml.g5.2xlarge",
+	container_startup_health_check_timeout=300,
+  )
+  
+# send request
+predictor.predict({
+	"inputs": "My name is Julien and I like to",
+})
